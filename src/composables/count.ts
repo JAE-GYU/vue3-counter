2615f72e79610ce9c89d82731fe978b471197c7b
JAE-GYU <shin3372@icloud.com>
2020-09-06 00:46:31 +0900

    refactor: Refactor doubleCount

--- src/composables/count.ts
+++ src/composables/count.ts
@@ -1,21 +1,21 @@
-import { reactive, Ref, toRefs, computed } from "vue";
+import { reactive, ComputedRef, toRefs, computed } from "vue";
 
 interface Count {
   count: number;
+  doubleCount: ComputedRef<number>;
 }
 
 export default function useCount() {
   const state = reactive<Count>({
-    count: 0
+    count: 0,
+    doubleCount: computed((): number => state.count * 2)
   });
-
-  const doubleCount: Ref<number> = computed(() => state.count * 2);
 
   const setCount = (x: number) => (state.count = x);
   const increment = () => state.count++;
   const decrement = () => state.count--;
 
-  const { count } = toRefs(state);
+  const { count, doubleCount } = toRefs(state);
 
   return {
     count,
